b64498abcfce295b90c7289faa6b38961e6d94fb
Elijah White <131682732+1ElijahW@users.noreply.github.com>
2023-07-10 13:38:25 -0400

    removed nested folder

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Routes, Route ,Navigate } from 'react-router-dom';
+
+import HomePage from '../src/pages/Home/HomePage';
+import LoginPage from '../src/pages/Login/LoginPage.js';
+import PollPage from '../src/pages/Poll/PollPage.js';
+import SignupPage from '../src/pages/Signup/SignupPage.js';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/Poll.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Poll = ({ poll }) => {
+  const { title, choiceA, choiceB, deadline, category } = poll;
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Poll;
