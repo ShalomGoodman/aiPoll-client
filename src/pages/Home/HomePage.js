3237e41185ea65bb6f0cec3c21ab1822dc4d3569
Alison <alison.bayona@gmail.com>
2023-07-10 15:43:52 -0400

    Updated Pages

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+// import CreatePoll from './CreatePoll'; // Assuming the file path is correct
 
 function HomePage() {
+  const [polls, setPolls] = useState([]);
+
+  useEffect(() => {
+    // Fetch polls on component mount
+    fetchPolls();
+  }, []);
+
+  const fetchPolls = async () => {
+    try {
+      const response = await axios.get('/all-polls/');
+      setPolls(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handlePollCreated = (newPoll) => {
+    // Update the list of polls with the newly created poll
+    setPolls((prevPolls) => [...prevPolls, newPoll]);
+  };
+
   return (
     <div>
-      <h1>Welcome to the Voting Poll App!</h1>
-      {/* Add your homepage content here */}
+      {/* <CreatePoll onPollCreated={handlePollCreated} /> */}
+
+      {/* Render the polls */}
+      {polls.map((poll) => (
+        <div key={poll.id}>
+          <h2>{poll.title}</h2>
+          {/* Render other poll details */}
+        </div>
+      ))}
     </div>
   );
 }
--- src/pages/Signup/SignupPage.js
+++ src/pages/Signup/SignupPage.js
@@ -1,12 +1,85 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./signup.css";
+import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 
-function SignupPage() {
+export default function SignupPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [verifyPassword, setVerifyPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // Perform your signup logic here
+      // Make API requests, validations, or any other necessary operations
+
+      setName("");
+      setEmail("");
+      setPassword("");
+      setVerifyPassword("");
+      toast.success("Successfully signed up!", { autoClose: 1500 });
+      navigate("/signin");
+    } catch (error) {
+      toast.error("An error occurred", { autoClose: 1500 });
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
-      <h2>Signup Page</h2>
-      {/* Add your signup form here */}
+    <div className="sign-up">
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            placeholder="username..."
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            type="email"
+            placeholder="email..."
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            type="password"
+            placeholder="password..."
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            type="password"
+            placeholder="verify password..."
+            value={verifyPassword}
+            onChange={(event) => setVerifyPassword(event.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Sign Up</button>
+      </form>
+      <span className="goTo">
+        {"Already have an account? Click "}
+        <Link className="link" to="/signin">
+          here
+        </Link>
+        {" to sign in instead."}
+      </span>
+      <ToastContainer />
     </div>
   );
 }
 
-export default SignupPage;
+
