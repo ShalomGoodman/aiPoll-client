a6754da3c3eebd555ccb753c27a12cb5e7a438b9
Elijah White <131682732+1ElijahW@users.noreply.github.com>
2023-07-10 15:10:19 -0400

    updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+
 function Navbar() {
   return (
     <nav className='Navbar'>
       <div className="Nav-logo">
         {/* Logo */}
       </div>
-      <ul className="Nav-button">
+      <ul className="Nav-logout">
         <li><button>Log out</button></li>
       </ul>
     </nav>
