d9f855e79fbcf758c04465d96e4e458b2bbf3b77
Elijah White <131682732+1ElijahW@users.noreply.github.com>
2023-07-12 09:39:47 -0400

    sync

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,11 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Poll.css'; // Import the CSS file
 
 const Poll = ({ poll }) => {
-  const { title, choiceA, choiceB, deadline, category } = poll;
+  const [pollData, setPollData] = useState({
+    id: 1,
+    user: '',
+    title: '',
+    option_a_label: '',
+    option_a_votes: 0,
+    option_b_label: '',
+    option_b_votes: 0,
+    created: '2023-07-11T19:28:25.362479Z',
+    winner: null,
+    voting_status: 'open',
+    duration_minutes: 0,
+    creator: 1,
+    chatbox: 1
+  });
+  const [selectedChoice, setSelectedChoice] = useState('');
+
+  useEffect(() => {
+    // Fetch poll data from the backend API
+    const fetchPollData = async () => {
+      try {
+        const response = await axios.get(`/api/polls/${poll.id}/`);
+        setPollData(response.data);
+      } catch (error) {
+        console.error('Error fetching poll data:', error);
+      }
+    };
+
+    fetchPollData();
+  }, [poll.id]);
+
+  const handleChoiceChange = (event) => {
+    setSelectedChoice(event.target.value);
+  };
+
+  const handleVote = () => {
+    console.log(selectedChoice);
+    // Add your logic here to submit the vote to the backend
+  };
+
+  const handleOptionAClick = () => {
+    setSelectedChoice('A');
+    handleVote();
+  };
+
+  const handleOptionBClick = () => {
+    setSelectedChoice('B');
+    handleVote();
+  };
 
   return (
-    <div>
+    <div className="poll-container">
+      <h2 className="poll-title">{pollData.title}</h2>
+      <p className="poll-category">{pollData.category}</p>
+      <p className="poll-deadline">Deadline: {pollData.deadline}</p>
 
+      <div className="poll-form">
+        <div className="poll-choice">
+          <label>
+            <input
+              type="radio"
+              value="A"
+              checked={selectedChoice === 'A'}
+              onChange={handleChoiceChange}
+            />
+            <span onClick={handleOptionAClick} className="poll-option-label">
+              {pollData.option_a_label}
+            </span>
+          </label>
+          <p className="poll-votes">Votes: {pollData.option_a_votes}</p>
+        </div>
+
+        <div className="poll-choice">
+          <label>
+            <input
+              type="radio"
+              value="B"
+              checked={selectedChoice === 'B'}
+              onChange={handleChoiceChange}
+            />
+            <span onClick={handleOptionBClick} className="poll-option-label">
+              {pollData.option_b_label}
+            </span>
+          </label>
+          <p className="poll-votes">Votes: {pollData.option_b_votes}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Poll/PollPage.js
+++ src/pages/Poll/PollPage.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import './pollpage.css';
 
 function PollPage() {
+  const { id } = useParams();
+  const [poll, setPoll] = useState(null);
+
+  useEffect(() => {
+    fetchPoll();
+  }, []);
+
+  const fetchPoll = async () => {
+    try {
+      // Make the API request to our Django Web3 API endpoint
+      const response = await axios.get(`/api/polls/${id}`);
+      setPoll(response.data);
+    } catch (error) {
+      console.error('Error fetching poll:', error);
+    }
+  };
+
+  if (!poll) {
+    return <div>Blah blahhh...</div>;
+  }
+
   return (
-    <div>
+    <div className="pollpage-container">
       <h2>Poll Page</h2>
-      {/* Add your poll component here */}
+      <h3 className="poll-title">{poll.title}</h3>
+      <p className="poll-option">Option 1: {poll.option1}</p>
+      <p className="poll-option">Option 2: {poll.option2}</p>
+      <p className="poll-time">
+        Time: {poll.time.days} days, {poll.time.hours} hours, {poll.time.minutes} minutes
+      </p>
+      <div className="poll-details">
+        {/* ??? ANY */}
+      </div>
     </div>
   );
+  
 }
 
 export default PollPage;
